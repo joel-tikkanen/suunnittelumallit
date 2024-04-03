30ce3dcfb299b751049a77a5914b53670c917eb7
zzokku <93818329+zzokku@users.noreply.github.com>
2024-04-03 19:20:46 +0300

    adapter

---
+++ adapter/src/App.java
@@ -0,0 +1,45 @@
+class VanhaJärjestelmä implements vanhaRajapinta {
+    public String vanhaToiminto() {
+        return "Vanha järjestelmä toimii!";
+    }
+}
+
+
+interface vanhaRajapinta  {
+    String vanhaToiminto();
+}
+
+interface UusiRajapinta {
+    String toiminto();
+}
+
+class Normitoteutus implements UusiRajapinta {
+    @Override
+    public String toiminto() {
+        return "Uusi järjestelmä toimii normaalisti!";
+    }
+}
+
+class Adapteri implements UusiRajapinta {
+    private vanhaRajapinta vanhaJärjestelmä;
+
+    public Adapteri(vanhaRajapinta vanhaJärjestelmä) {
+        this.vanhaJärjestelmä = vanhaJärjestelmä;
+    }
+
+    @Override
+    public String toiminto() {
+        return vanhaJärjestelmä.vanhaToiminto();
+    }
+}
+
+public class App {
+    public static void main(String[] args) {
+        vanhaRajapinta vanha = new VanhaJärjestelmä();
+        UusiRajapinta adapteri = new Adapteri(vanha);
+        UusiRajapinta normaali = new Normitoteutus();
+
+        System.out.println(normaali.toiminto()); 
+        System.out.println(adapteri.toiminto()); 
+    }
+}
